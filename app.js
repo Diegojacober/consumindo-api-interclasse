04fbfc4d019a246cc8afd31419f6a0296a20874f
Diego Alencar Jacober <57761933898>
2023-04-10 16:29:47 -0300

    Configurando sliders

--- app.js
+++ app.js
@@ -1,75 +1,158 @@
-async function getJogos(){ 
+let quantidade = 1
+async function getJogos() {
     let url = `http://localhost:8000/api/v1/jogos/today`
 
     let results = await fetch(url)
     let json = await results.json()
-    
-    console.log(json)
-    jogos = document.getElementById('jogos')
-    json.forEach(element => {   
-        console.log(element)
-        divCard = document.createElement("div")
-        divCard.classList.add("card-jogo");
+
+
+    json.forEach(element => {
         
-        lado1 = document.createElement("div")
-        lado1.classList.add("lado1")
+        let carrouselInner = document.getElementsByClassName("carousel-inner")
+        var carrouselItem = document.getElementsByClassName("carousel-item")
+        let lastCarrouitem = carrouselItem[carrouselItem.length - 1]
+        if (quantidade % 2 != 0 && quantidade != 1) {
+            // cria um carrousel item
+            let divCarrouselItem = document.createElement("div")
+            divCarrouselItem.classList.add("carousel-item");
+            divCarrouselItem.classList.add("p-4");
+            divCarrouselItem.setAttribute("data-bs-interval", "7000");
 
-        p1 = document.createElement("p")
-        p1.classList.add("title--team")
-        p1.innerHTML = `${element.time1_nome}`
+            // cria o card
+            divCard = document.createElement("div")
+            divCard.classList.add("card-jogo");
 
-        lado1.appendChild(p1)
+            lado1 = document.createElement("div")
+            lado1.classList.add("lado1")
 
-        divCard.appendChild(lado1)
+            p1 = document.createElement("p")
+            p1.classList.add("title--team")
+            p1.innerHTML = `${element.time1_nome}`
+
+            lado1.appendChild(p1)
+
+            divCard.appendChild(lado1)
 
 
-        divVs = document.createElement("div")
-        divVs.classList.add("vs")
+            divVs = document.createElement("div")
+            divVs.classList.add("vs")
 
-        divImagem = document.createElement("img")
-        divImagem.setAttribute("src", "./imgs/versus.png");
-        divImagem.setAttribute("alt", "Imagem de versus");
+            spanModalidade = document.createElement("span")
 
-        divVs.appendChild(divImagem)
+            spanModalidade.classList.add("game--modalidade")
 
-        dataJogo = element.data_do_jogo
-        dataJogo = dataJogo.split(" ");
+            spanModalidade.innerHTML = `${element.modalidade}`.toUpperCase()
 
-        horasEMinuto = dataJogo[1].split(":")
+            divVs.appendChild(spanModalidade)
 
-        spanHora = document.createElement("span")
+            divImagem = document.createElement("img")
+            divImagem.setAttribute("src", "./imgs/versus.png");
+            divImagem.setAttribute("alt", "Imagem de versus");
 
-        spanHora.classList.add("game--time")
+            divVs.appendChild(divImagem)
 
-        spanHora.innerHTML = `${horasEMinuto[0]}:${horasEMinuto[1]}`
+            dataJogo = element.data_do_jogo
+            dataJogo = dataJogo.split(" ");
 
-        divVs.appendChild(spanHora)
+            horasEMinuto = dataJogo[1].split(":")
 
-        divCard.appendChild(divVs)
+            spanHora = document.createElement("span")
 
-        lado2 = document.createElement("div")
-        lado2.classList.add("lado2")
-        p2 = document.createElement("p")
-        p2.classList.add("title--team")
-        p2.innerHTML = `${element.time2_nome}`
-        lado2.appendChild(p2)
-        divCard.appendChild(lado2)
+            spanHora.classList.add("game--time")
 
-        jogos.append(divCard)
+            spanHora.innerHTML = `${horasEMinuto[0]}:${horasEMinuto[1]}`
+
+            divVs.appendChild(spanHora)
+
+            divCard.appendChild(divVs)
+
+            lado2 = document.createElement("div")
+            lado2.classList.add("lado2")
+            p2 = document.createElement("p")
+            p2.classList.add("title--team")
+            p2.innerHTML = `${element.time2_nome}`
+            lado2.appendChild(p2)
+            divCard.appendChild(lado2)
+
+            divCarrouselItem.appendChild(divCard)
+            carrouselInner[0].append(divCarrouselItem)
+            
+            quantidade++
+        } else {
+            //coloca no ultimo carrousel item
+            divCard = document.createElement("div")
+            divCard.classList.add("card-jogo");
+
+            lado1 = document.createElement("div")
+            lado1.classList.add("lado1")
+
+            p1 = document.createElement("p")
+            p1.classList.add("title--team")
+            p1.innerHTML = `${element.time1_nome}`
+
+            lado1.appendChild(p1)
+
+            divCard.appendChild(lado1)
+
+
+            divVs = document.createElement("div")
+            divVs.classList.add("vs")
+
+            spanModalidade = document.createElement("span")
+
+            spanModalidade.classList.add("game--modalidade")
+
+            spanModalidade.innerHTML = `${element.modalidade}`.toUpperCase()
+
+            divVs.appendChild(spanModalidade)
+
+            divImagem = document.createElement("img")
+            divImagem.setAttribute("src", "./imgs/versus.png");
+            divImagem.setAttribute("alt", "Imagem de versus");
+
+            divVs.appendChild(divImagem)
+
+            dataJogo = element.data_do_jogo
+            dataJogo = dataJogo.split(" ");
+
+            horasEMinuto = dataJogo[1].split(":")
+
+            spanHora = document.createElement("span")
+
+            spanHora.classList.add("game--time")
+
+            spanHora.innerHTML = `${horasEMinuto[0]}:${horasEMinuto[1]}`
+
+            divVs.appendChild(spanHora)
+
+            divCard.appendChild(divVs)
+
+            lado2 = document.createElement("div")
+            lado2.classList.add("lado2")
+            p2 = document.createElement("p")
+            p2.classList.add("title--team")
+            p2.innerHTML = `${element.time2_nome}`
+            lado2.appendChild(p2)
+            divCard.appendChild(lado2)
+
+            lastCarrouitem.append(divCard)
+            quantidade++
+        }
+
 
     });
-    
+
 }
 
-function getDay(){
+function getDay() {
     h6 = document.getElementById('dia-atual')
     let data = new Date()
     let ano = data.getFullYear()
-    let mes =  String(data.getMonth() + 1).padStart(2, '0');
+    let mes = String(data.getMonth() + 1).padStart(2, '0');
     let dia = String(data.getDate()).padStart(2, '0');
     h6.innerHTML = `${dia} / ${mes} / ${ano}`
 }
 
 getDay()
 getJogos()
-        
+
